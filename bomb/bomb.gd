a3319cae6ac5041600aaddd919397dd6770678eb
GildedBirch <thebirchbreaker@gmail.com>
2025-11-22 22:30:24 +0200

    Sound effects for bomb

--- bomb/bomb.gd
+++ bomb/bomb.gd
@@ -4,6 +4,12 @@ extends RigidBody3D
 
 @onready var fuse_timer: Timer = %FuseTimer
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var audio_player: AudioStreamPlayer3D = %AudioStreamPlayer3D
+const EXPLOSION_AUDIO: Array[AudioStreamWAV] = [
+	preload("uid://dwwhg36dd11b0"),
+	preload("uid://be4hcif6wb1bq"),
+	preload("uid://31a7xstnolso"),
+]
 
 
 func _ready() -> void:
@@ -11,6 +17,9 @@ func _ready() -> void:
 
 
 func _on_fuse_timeout() -> void:
+	freeze = true
 	animation_player.play(&"explode")
+	audio_player.stream = EXPLOSION_AUDIO.pick_random()
+	audio_player.play()
 	await animation_player.animation_finished
 	queue_free()
