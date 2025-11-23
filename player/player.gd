a5f602b5bcf76d64e28352319dbfd0bb16cec40b
GildedBirch <thebirchbreaker@gmail.com>
2025-11-23 05:10:30 +0200

    Throwing and placing bombs

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,11 @@ class_name Player
 extends CharacterBody3D
 
 
+const BOMB = preload("uid://dgsfjbgrlgh3e")
+enum BombState {
+	NONE,
+	HELD,
+	}
 @export var speed: float = 5.0
 @export var deceleration_force: float = 1.0
 var apply_gravity: bool = true
@@ -9,11 +14,14 @@ var can_move: bool = true
 var mouse_free: bool = false
 var _mouse_sens: float = 0.0
 var _controller_sens: float = 0.0
+var _bomb_state: BombState = BombState.NONE
+var current_bomb: Bomb
 @onready var mesh_pivot: Node3D = %MeshPivot
 @onready var camera_arm: SpringArm3D = %CameraArm
 @onready var camera_3d: Camera3D = %Camera3D
 @onready var camera_pivot: Node3D = %CameraPivot
 @onready var look_at_3d: LookAt3D = %LookAt3D
+@onready var bomb_marker: Marker3D = %BombMarker
 
 
 func _ready() -> void:
@@ -23,8 +31,8 @@ func _ready() -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed(&"debug_quit"):
-		get_tree().quit()
+	if event is InputEventKey or event is InputEventMouseButton:
+		_handle_input(event)
 	
 	if event is InputEventMouseMotion:
 		if mouse_free:
@@ -65,6 +73,44 @@ func is_moving() -> bool:
 	if Input.get_vector(&"left", &"right", &"forward", &"backward") != Vector2.ZERO:
 		return true
 	return false
+
+
+func _handle_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"debug_quit"):
+		get_tree().quit()
+	if event.is_action_pressed(&"spawn_bomb"):
+		_spawn_bomb()
+	if event.is_action_pressed(&"place_bomb"):
+		_place_bomb()
+	if event.is_action_pressed(&"throw_bomb"):
+		_throw_bomb()
+
+
+func _spawn_bomb() -> void:
+	if not _bomb_state == BombState.NONE:
+		return
+	current_bomb = BOMB.instantiate()
+	bomb_marker.add_child(current_bomb)
+	_bomb_state = BombState.HELD
+	
+
+
+func _place_bomb() -> void:
+	if not _bomb_state == BombState.HELD:
+		return
+	current_bomb.fuse_timer.start()
+	current_bomb.reparent(get_tree().root)
+	current_bomb.global_position = global_position + (mesh_pivot.basis * Vector3.FORWARD)
+	_bomb_state = BombState.NONE
+
+
+func _throw_bomb() -> void:
+	if not _bomb_state == BombState.HELD:
+		return
+	current_bomb.fuse_timer.start()
+	current_bomb.reparent(get_tree().root)
+	current_bomb.apply_impulse(((mesh_pivot.basis * Vector3.FORWARD) + Vector3.UP) * 4)
+	_bomb_state = BombState.NONE
 
 
 func _on_sensitivity_changed() -> void:
