c1d749e8ecb64c5a52d8a530da094a04b58432c5
GildedBirch <thebirchbreaker@gmail.com>
2025-11-25 04:30:46 +0200

    Scenery and enemy improvements

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -9,12 +9,16 @@ var is_navigating: bool = false
 var _is_stop_1: bool = true
 @onready var nav_agent: NavigationAgent3D = %NavigationAgent3D
 @onready var bomb_target_component: BombTargetComponent = %BombTargetComponent
+@onready var player_detector: Area3D = %PlayerDetector
+@onready var timer: Timer = %Timer
+@onready var mesh_pivot: Node3D = %MeshPivot
 
 
 func _ready() -> void:
 	bomb_target_component.got_hit.connect(_got_hit)
 	nav_agent.navigation_finished.connect(_on_nav_target_reached)
 	nav_agent.velocity_computed.connect(_on_nav_velocity_computed)
+	timer.timeout.connect(_on_timeout)
 	update_target_location(stop_1.global_position)
 
 
@@ -23,7 +27,9 @@ func _physics_process(delta: float) -> void:
 		return
 	var current_location: Vector3 = global_position
 	var next_location: Vector3 = nav_agent.get_next_path_position()
-	var new_velocity: Vector3 = (next_location - current_location).normalized() * speed
+	var new_velocity: Vector3 = Vector3.ZERO
+	new_velocity.x = (next_location - current_location).normalized().x * speed
+	new_velocity.z = (next_location - current_location).normalized().z * speed
 	nav_agent.velocity += get_gravity() * 2.0 * delta
 	nav_agent.velocity = new_velocity
 
@@ -50,3 +56,13 @@ func _on_nav_target_reached() -> void:
 
 func _got_hit(_damage: int) -> void:
 	queue_free()
+
+
+func _on_timeout() -> void:
+	var players: Array[Node3D] = player_detector.get_overlapping_bodies()
+	for p in players:
+		print(p.name)
+	if not players:
+		return
+	#print("a")
+	mesh_pivot.look_at(players[0].global_position)
